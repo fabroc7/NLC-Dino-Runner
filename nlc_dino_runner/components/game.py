48e2b210f56bebf7995704721c6618f063fac0b4
Fabián Romero Claros <fabian130404@gmail.com>
2021-09-09 18:55:51 -0500

    Clase 3: Dino

--- nlc_dino_runner/components/game.py
+++ nlc_dino_runner/components/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from nlc_dino_runner.components.dinosaur import Dinosaur
 from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_WIDTH, SCREEN_HEIGHT, BG, FPS
 
 
@@ -11,9 +12,10 @@ class Game:
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.playing = False
+        self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 360
-        self.game_speed = 20
+        self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -29,12 +31,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
